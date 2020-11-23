3f3a4417bc11528829d1a30f3b411d1ffebf9546
amchicken <yooradet.khamon@hotmail.com>
2020-11-23 22:38:33 +0700

    w8mylogopls

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React from 'react';
 
 const Reservation = ({isReservationClose,setIsReservationClose}) => {
     const closeHandle = () => setIsReservationClose(!isReservationClose);
